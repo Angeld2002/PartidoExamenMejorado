1a4f24e892b04321ef8e008c64e7c67594c54f33
Angeld2002 <2002angeldavid@gmail.com>
2023-12-14 20:54:56 +0100

    cambio de version 33 a 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.politecnicomalaga.partidoexamen"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.politecnicomalaga.partidoexamen"
